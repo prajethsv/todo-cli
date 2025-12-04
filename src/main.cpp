a522c27136c6863833b6834ac1bcd48153d09d94
prajethsv <prajethsuresh@gmail.com>
2025-12-04 19:53:05 +0800

    Added new functions and header; updated main

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+/* 
+The user interacts through a menu in the terminal,
+ choosing actions like “add task,” “view tasks,”
+  or “delete task.” The program stores all tasks in memory while running,
+   and whenever the user exits, it writes them into a text file. When the program starts again,
+    it reads that file and loads all the previous tasks back in.
+    */
