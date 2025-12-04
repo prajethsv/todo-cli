65523d372df5f160fbc4dec9963559317a3178dd
prajethsv <prajethsuresh@gmail.com>
2025-12-04 20:28:18 +0800

    Added Class Variables And Get Methods and Constructors

--- src/header.hpp
+++ src/header.hpp
@@ -8,8 +8,56 @@ class TaskInfo {
     private:
     
     string task;
-    int date
-  
+    int day;
+    int month;
+    int year;
+
+    int hour;
+    int minute;
     
-    
+    public: 
+
+    TaskInfo(){
+
+        task = "";
+        day = 0;
+        month = 0;
+        year = 0;
+
+        hour = 0;
+        minute = 0;
+
+    }
+
+    TaskInfo(string t, int d, int m, int y, int h, int mi){
+
+        task = t;
+        day = d;
+        month = m;
+
+        year = y;
+        hour = h;
+
+    }
+
+    string getTask(){
+        return task;
+    }
+
+    int getDay(){
+        return day;
+    }
+
+    int getMonth(){
+        return month;
+    }
+
+    int getYear(){
+        return year;
+    }
+
+    int getHour(){
+        return hour;
+    }
+
 };
